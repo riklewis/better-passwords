816a863cf1900a989d7f7281eb1465cc0fea8627
Rik Lewis <rik@riklewis.com>
2020-11-27 23:20:08 +0000

    Bump PHP and Wordpress versions

--- svn/trunk/better-passwords.php
+++ svn/trunk/better-passwords.php
@@ -130,10 +130,10 @@ function better_pass_show_settings() {
 function better_pass_badge_php() {
   $ver = better_pass_phpversion();
   $col = "critical";
-  if(version_compare($ver,'7.2','>=')) {
+  if(version_compare($ver,'7.3','>=')) {
     $col = "important";
   }
-  if(version_compare($ver,'7.3','>=')) {
+  if(version_compare($ver,'7.4','>=')) {
     $col = "success";
   }
   return 'https://img.shields.io/badge/PHP-' . $ver . '-' . $col . '.svg?logo=php&style=for-the-badge';
